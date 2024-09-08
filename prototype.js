550a503c6c05402a7f341da9da9de5e663a3850d
Abhishek-K-Vishvakarma <github101@gmail.com>
2024-09-08 15:48:55 +0530

    Add new prototype

--- prototype.js
+++ prototype.js
@@ -4,7 +4,7 @@ function FunName(){
           this.name = name;
           this.title = title;
           this.age  = age;
-          this.city = city;
+          this.city = city;   
      }
      
      this.data = function(){
@@ -19,4 +19,41 @@ let list = new FunName();
 list.item('Abhishek', 'Vishvakarma', 23, 'Varanasi');
 list.data();
 
+// With arrow function >>>>>>>>>>>>>>>>>>>>
 
+function FunName_name() {
+
+     this.item_data = (name, title, age, city) => {
+          this.name = name;
+          this.title = title;
+          this.age = age;
+          this.city = city;
+     }
+
+     this.data_item = () =>  {
+          console.log(this.name);
+          console.log(this.title);
+          console.log(this.age);
+          console.log(this.city);
+     };
+};
+
+let list_data = new FunName_name();
+list_data.item_data('Abhi', 'Vishvakarma', 21, 'Varanasi');
+list_data.data_item();
+
+
+//  Second method of Prototype >>>>>>>>>>>>
+
+function Funname(firstName, lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+        Funname.prototype.getFullData = function(){
+          return this.firstName + " " + this.lastName;
+        }
+}
+
+let all_list_of_data = new Funname('Peter', 'Parker');
+
+console.log(all_list_of_data.getFullData());
